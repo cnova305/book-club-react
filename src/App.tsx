680d9ef7b8d708dd37d61cc2f247bc51f2fb1c92
Christopher Juwao <njuwao@icloud.com>
2022-05-24 19:02:28 +0200

    Home Page Design

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import './App.css';
 
+// Importing Pages
+import Home from './pages/Home/Home'
+
 function App() {
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <Home />
     </div>
   );
 }
---
+++ src/pages/Home/Home.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+
+import './Home.css'
+
+import homeImage from '../../assets/Home.svg'
+
+const Home = () => {
+  return (
+      <>
+        <div className='pageContainer'>
+            <div className='contentContainer'>
+                <div className='homeImageContainer'>
+                    <img src={homeImage} alt='home' />
+                </div>
+                <div className='homeTitleContainer'>
+                    <h1>Welcome To Avid Readers Book Club</h1>
+                    <div className='homeButtonsContainer'>
+                        <button>Sign Up</button>
+                        <button>Sign Up</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+      </>
+    
+  )
+}
+
+export default Home
