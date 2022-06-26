aa7f93cbb81c8f0f7d55c9dc5a0717976fbf19f1
Christopher Juwao <njuwao@icloud.com>
2022-06-26 18:13:34 +0200

    add book image functionality

--- src/components/BookCardForm/BookCardForm.tsx
+++ src/components/BookCardForm/BookCardForm.tsx
@@ -4,8 +4,12 @@ import './BookCardForm.css'
 
 import { Book } from '../../types/types';
 
-import { database } from '../../config/firebase';
+import { database, storage } from '../../config/firebase';
+
 import { addDoc, collection, doc, setDoc } from "firebase/firestore";
+import { ref, uploadBytesResumable, getDownloadURL } from "firebase/storage";
+
+import  { v1 } from 'uuid';
 
 const BookCardForm = () => {
 
@@ -16,11 +20,43 @@ const BookCardForm = () => {
         author: '',
         genre: '',
         year: '',
+        image: '',
         user: {
             name: 'nova',
             uid: 'iUp8K7fsVDanijOetuv38eGCVoD2',
         }
     })
+
+    // Setting Up Image State
+
+    const [bookimage, setBookimage] = useState(null)
+
+    // Function to Preview Book Image
+
+    const previewBookImage = (e:any) => {
+      if (e.target.files && e.target.files.length > 0) {
+        setBookimage(e.target.files[0])
+      }
+    }
+
+    // Function to Remove Book Image Preview
+
+    const removeBookImage = (e:any) => {
+      setBookimage(null);
+    }
+
+    // funnction to upload Book Image
+
+    const uploadBookImage = (e:any) => {
+      if (bookimage != null) {
+        const bookImageRef = ref(storage, `books/${v1()}`)
+        uploadBytesResumable(bookImageRef, bookimage).then(() => {
+          alert('Image')
+        }
+        )
+      }
+
+    }
 
     // Setting Book Update Function
 
@@ -43,7 +79,24 @@ const BookCardForm = () => {
   return (
     <div className='bookcardForm'>
       <div className='bookcardFormImage'>
-        book card
+        {bookimage && (
+          <div>
+            <img 
+              src={URL.createObjectURL(bookimage)}
+              alt='book' />
+          </div>
+        )}
+        <input 
+          accept='image/*'
+          type='file' 
+          onChange={previewBookImage}
+        />
+        <button onClick={removeBookImage}>
+        Delete
+      </button>
+      <button onClick={uploadBookImage}>
+        Upload
+      </button>
       </div>
       <div className='bookcardFormDetails'>
         <form onSubmit={submitBook}>
--- src/types/types.ts
+++ src/types/types.ts
@@ -14,4 +14,5 @@ export interface Book {
     genre: string;
     user: User;
     year: string;
+    image: string;
 }
